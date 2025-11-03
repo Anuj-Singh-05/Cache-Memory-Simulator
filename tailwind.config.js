950db0901fb09aee492dad5bbf46514737ed8376
Anuj Singh <anujkrsingh2926@gmail.com>
2025-11-03 21:27:04 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0f1724',
+        accent: '#06b6d4',
+        accent2: '#7c3aed'
+      }
+    }
+  },
+  plugins: [],
+}
